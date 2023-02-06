b42e94fd84e0297ca9950cd8e916617414a091f2
Petri <peewter@gmail.com>
2023-02-06 21:31:24 +0200

    Update layout

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ const App = () => {
       <div className='App'>
         <h1>...SPEED KILLS...</h1>
         <p>Upcoming events: </p>
-        <p>4/3/2023 Varjobaari</p>
-        <p>12/3/2023 Dog's Home</p>
+        <p>4/3/2023 Varjobaari, Tampere</p>
+        <p>12/3/2023 Dog's Home, Tampere</p>
         <Social />
       </div>
+      <div className='line'>X</div>
     </div>
   );
 };
